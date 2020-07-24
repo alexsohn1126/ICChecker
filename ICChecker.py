61621c650c2476e60ead0ed8a3e2a491d7d6d711
Alex Sohn <44201357+alexsohn1126@users.noreply.github.com>
2020-07-24 01:31:22 -0400

    Added methods and functions
    Changed IC class's name to InterestCheck.
    Added export_to_json for InterestCheck class. Calling this method on an InterestCheck instance will make a file called 'interest_check.json' if not already there. If it is, it will add json object to the json file.
    Added get_posts function. it takes Geekhack's interest check page and extracts links for all Interest checks.

--- ICChecker.py
+++ ICChecker.py
@@ -1,10 +1,10 @@
 import requests
-from typing import List, Tuple
+from typing import List
 from bs4 import BeautifulSoup as bs
+import json
 import re
 
-
-class IC:
+class InterestCheck:
 	"""
 	A general interest check related to keyboards.
 
@@ -13,32 +13,44 @@ class IC:
 		Beautiful soup object for the IC
 	gbDate:
 		Group Buy Date (if available)
-	picURL:
-		URL for the first picture included in the post
+	picsURL:
+		URL for the pictures included in the post
 	"""
 
 	def __init__(self, url: str) -> None:
-		self.page = bs(requests.get(url).content, 'html.parser')
+		self.url = url
+		self.post = bs(requests.get(url).content,
+						'html.parser').find(class_="inner")
+		self.pics_url = [img.get('src') for img in self.post.find_all('img')]
 		self.gbDate = None
-		self.picURL = ''
 		self.price = None
 
+	def export_to_json(self) -> None:
+		ic_info_json = open('interest_check.json', 'a')
+		ic_info_dict = {
+			'url': self.url,
+			'pics_url': self.pics_url,
+			'gbDate': None,
+			'price': None
+		}
+		json.dump(ic_info_dict, ic_info_json, indent = 4)
+		ic_info_json.close()
+
 	def _isolate_text(self) -> str:
-		first_post = self.page.find(class_="inner")
-		first_post_text = first_post.get_text()
+		first_post_text = self.post.get_text()
 		return first_post_text
 
-	def find_details(self):
-		post_text = self._isolate_text()
+	# def find_details(self):
+	# 	post_text = self._isolate_text()
 
-		# find Groupbuy date
-		for month in ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
-					 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'):
-			match_month_date = re.search(month + '[1-3][0-9]|[1-9]', post_text)
-			match_date_month = re.search('([1-3][0-9]|[1-9]) ', post_text)
+	# 	# find Groupbuy date
+	# 	for month in ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
+	# 				 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'):
+	# 		match_month_date = re.search(month + '[1-3][0-9]|[1-9]', post_text)
+	# 		match_date_month = re.search('([1-3][0-9]|[1-9]) ', post_text)
 
 
-class KeyboardIC(IC):
+class KeyboardIC(InterestCheck):
 	def __init__(self, url: str):
 		IC.__init__(self, url)
 		self.keyboardName = None
@@ -46,7 +58,7 @@ class KeyboardIC(IC):
 		self.keyboardMount = None
 
 
-class KeycapIC(IC):
+class KeycapIC(InterestCheck):
 	def __init__(self, url: str):
 		IC.__init__(self, url)
 		self.profile = None
@@ -54,3 +66,23 @@ class KeycapIC(IC):
 	def find_details(self):
 		pass
 
+def get_posts(url: str) -> List[str]:
+	ic_list_page = bs(requests.get(url).content, 'html.parser')
+	ic_list_links = []
+	# search for a html tag with 6 or 5 digits followed by .0, and has no class
+	# link with 36672 is the rules for IC section, thus ignored.
+	for ic in ic_list_page.find_all(href = re.compile(
+									'(?!36672)(\d{6}|\d{5})\.0$'),
+									class_= False):
+		ic_list_links.append(ic.get('href'))
+	return ic_list_links
+
+
+if __name__ == "__main__":
+	ic_first_list = get_posts('https://geekhack.org/index.php?board=132.0')
+	for ic in ic_first_list:
+		print(ic)
+	# KIC = InterestCheck('https://geekhack.org/index.php?topic=105440.0')
+	# for picURL in KIC.pics_url:
+	# 	print(picURL)
+	# KIC.export_to_json()
